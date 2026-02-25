cb151ff8db81e860675e14e6d9876b21faab22d8
lucia-catu <cat24206@uvg.edu.gt>
2026-02-25 09:38:52 -0600

    Update main.asm

--- lab03/lab03/main.asm
+++ lab03/lab03/main.asm
@@ -7,6 +7,7 @@
 */
 /**************/
 // Encabezado (Definición de Registros, Variables y Constantes)
+
 .include "m328pdef.inc"
 .dseg
 .org    SRAM_START
@@ -14,82 +15,66 @@
 //variable_name:     .byte   1   // Memory alocation for variable_name:     .byte   (byte size)
 .cseg
 .org 0x0000
+	rjmp SETUP
 
-rjmp start
-
-.org PCI1addr								; habilito la interrupción para los pines del portC
-RJMP IncDec_4leds
+.org 0x001C
+    rjmp T0_ISR
 
  /**************/
 // Configuración de la pila
-
-start:
 LDI     R16, LOW(RAMEND)
 OUT     SPL, R16
 LDI     R16, HIGH(RAMEND)
 OUT     SPH, R16
-
 /**************/
 
 	//TABLA
 Tabla:
     .db 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E
 
-
 /**************/
 // Configuración MCU
 
 SETUP:
+
     ldi ZH, high(Tabla<<1)
     ldi ZL, low(Tabla<<1)
 
 	LDI r16, 0x00							; Habilitar los pines 0 y 1 del puerto D.
 	STS UCSR0B, r16
 
-// Oscilador a 1MHz
-LDI R16, (1<<CLKPCE)						; Habilito la posibilidad de cambiar el oscilador
-STS CLKPR, R16								; aquí escribo el valor en el registro CLKPR
+	; PORTD salida (segmentos)
+    ldi r16, 0xFF
+    out DDRD, r16
 
-LDI R16, 0b0000_0100						; Configuro el prescaler a 16 (16MHz/16 = 1MHz)
-STS CLKPR, R16								; Escribo ese valor en el registro CLKPR y con eso ya cambié el oscilador.
-// 
+    ; PB0/PB1 salida (selección dígitos)
+    sbi DDRB, 0
+    sbi DDRB, 1
 
-; PORT B
-    ldi  r16, 0x0F             ; Salidas en el port B para los 4 leds
-    out  DDRB, r16
+    ; Estado inicial
+    cbi PORTB, 0
+    cbi PORTB, 1
+    ldi r16, 0xFF
+    out PORTD, r16
 
-	ldi r16, 0x00              ; Leds apagadas
-	out PORTB, r16
+	; Variables
+    clr r18   ; unidades
+    clr r19   ; decenas
+    clr r20   ; 0=unidades 1=decenas
+    clr r21
+    clr r22
 
-; PORTD como salida (display) 
-    ldi R16, 0xFF
-    out DDRD, R16
+	; Timer0: CTC, prescaler 64, OCR0A=249 (1ms)
+    ldi r16, (1<<WGM01)
+    out TCCR0A, r16
+    ldi r16, (1<<CS01)|(1<<CS00)
+    out TCCR0B, r16
+    ldi r16, 249
+    out OCR0A, r16
+    ldi r16, (1<<OCIE0A)
+    sts TIMSK0, r16
 
-    ldi R16, 0xFF   ; Apagar display 
-    out PORTD, R16
-
-; PORT C
-    cbi DDRC, PC0 ;Botones PC0 y PC1 como entrada 
-    cbi DDRC, PC1
-
-    sbi PORTC, PC0 ; Activar pull-ups
-    sbi PORTC, PC1
-
-// Configurar PCINT1
-LDI R16, (1<<PCIE1)
-STS PCICR, R16
-LDI R16, (1<<PCINT8)|(1<<PCINT9)
-STS PCMSK1, R16
-
-// CLEAR de mis contadores y variables 
-CLR R20
-CLR R21	
-IN R21, PINC			; estado previo de botones
-
-// ACTIVO INTERRUPCIONES GLOBALES
-SEI 
-
-	rjmp MAIN
+    sei
 
 /**************/
 	;LOOP PRINCIPAL
@@ -97,45 +82,84 @@ SEI
 	MAIN:
 	 rjmp MAIN
 
-/**************/
-// NON-Interrupt subroutines
 
 /**************/
-// Interrupt routines
+	; interrupciones
 
-IncDec_4leds:
-    PUSH R16
-    PUSH R17
-    IN   R16, SREG
-    PUSH R16
+T0_ISR:
+    ; SOLO guardar temporales / Z
+    push r16
+    push r30
+    push r31
 
-    IN   R16, PINC							 
-    MOV  R17, R16							 
+    ; Apagar ambos dígitos
+    cbi PORTB, 0
+    cbi PORTB, 1
 
-    EOR  R16, R21							 
-											 
-// Suma										 
-    SBRS R16, 0								 
-    RJMP resta								 
-    SBRS R17, 0								  
-    INC  R20
+    ; Mux
+    tst r20
+    brne SHOW_TENS
 
-resta:
-// Resta
-    SBRS R16, 1								 
-    RJMP mostrar_leds						 
-    SBRS R17, 1								  
-    DEC  R20								 
+SHOW_UNITS:
+    mov r16, r18
+    rcall LOAD_SEG
+    out PORTD, r16
+    sbi PORTB, 0          ; ON unidades
+    ldi r20, 1
+    rjmp COUNT_MS
 
-mostrar_leds:
-    ANDI R20, 0x0F							 
-    OUT  PORTB, R20
+SHOW_TENS:
+    mov r16, r19
+    rcall LOAD_SEG
+    out PORTD, r16
+    sbi PORTB, 1          ; ON decenas
+    clr r20
 
-    MOV  R21, R17							
+COUNT_MS:
+    ; ms++
+    inc r21
+    brne MS_CHECK
+    inc r22
 
-    POP  R16
-    OUT  SREG, R16
-    POP  R17
-    POP  R16
-    RETI
+MS_CHECK:
+    ; 1000 ms = 0x03E8
+    cpi r22, 0x03
+    brne EXIT_ISR
+    cpi r21, 0xE8
+    brne EXIT_ISR
 
+    clr r21
+    clr r22
+
+; +1 segundo (00..59)
+
+inc r18
+cpi r18, 10
+brlo EXIT_ISR        ; si unidades < 10 ? listo
+
+clr r18              ; si llegó a 10 ? volver a 0
+inc r19              ; subir decenas
+
+cpi r19, 6           ; ¿llegó a 6?
+brlo EXIT_ISR        ; si <6 ? listo (10..59)
+
+; si llegó a 60 ? reiniciar
+clr r19
+clr r18
+
+EXIT_ISR:
+    pop r31
+    pop r30
+    pop r16
+    reti
+
+; ----------------------------------------------------------
+; LOAD_SEG para display
+; ----------------------------------------------------------
+LOAD_SEG:
+    ldi ZH, high(Tabla<<1)
+    ldi ZL, low(Tabla<<1)
+    add ZL, r16
+    adc ZH, r1
+    lpm r16, Z
+    ret
