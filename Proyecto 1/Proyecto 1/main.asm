60f84e95164bb251b66b5e444ddcc071ea7d0b40
lucia-catu <cat24206@uvg.edu.gt>
2026-03-04 16:58:16 -0600

    inicio

---
+++ Proyecto 1/Proyecto 1/main.asm
@@ -0,0 +1,49 @@
+/*
+* Proyecto1.asm
+*
+* Created: 4/03/2026 16:44:58
+* Author : Lucia Catú
+*/
+/****************************************/
+// Encabezado (Definición de Registros, Variables y Constantes)
+.include "M328PDEF.inc"     // Include definitions specific to ATMega328P
+.dseg
+.org    SRAM_START
+//variable_name:     .byte   1   // Memory alocation for variable_name:     .byte   (byte size)
+
+.cseg
+.org 0x0000
+ /****************************************/
+// Configuración de la pila
+LDI     R16, LOW(RAMEND)
+OUT     SPL, R16
+LDI     R16, HIGH(RAMEND)
+OUT     SPH, R16
+/****************************************/
+
+//TABLA
+Tabla:
+    .db 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E
+
+
+// Configuracion MCU
+SETUP:
+
+	ldi ZH, high(Tabla<<1)
+    ldi ZL, low(Tabla<<1)
+
+	LDI r16, 0x00							; Habilitar los pines 0 y 1 del puerto D.
+	STS UCSR0B, r16
+    
+/****************************************/
+// Loop Infinito
+MAIN_LOOP:
+    RJMP    MAIN_LOOP
+
+/****************************************/
+// NON-Interrupt subroutines
+
+/****************************************/
+// Interrupt routines
+
+/****************************************/
