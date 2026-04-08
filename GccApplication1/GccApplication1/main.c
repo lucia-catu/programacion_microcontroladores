906928bbc9184fb8626ff07d71f11a5abaf7d3ec
lucia-catu <cat24206@uvg.edu.gt>
2026-04-07 22:33:22 -0600

    prelab4

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 19/03/2026 15:49:55
+ * Author : eacat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ GccApplication2/GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 7/04/2026 22:31:27
+ * Author : eacat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
