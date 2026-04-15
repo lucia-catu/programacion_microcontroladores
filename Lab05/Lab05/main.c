85766453232bc91fd1d9ef128da491826da0e4fb
lucia-catu <cat24206@uvg.edu.gt>
2026-04-15 14:39:36 -0600

    lab 5

---
+++ Lab05/Lab05/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab05.c
+ *
+ * Created: 15/04/2026 14:35:46
+ * Author : eacat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
