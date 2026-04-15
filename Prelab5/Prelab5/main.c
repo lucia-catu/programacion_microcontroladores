25f8d78826a50410413fb94dd57dfa49a52f68d6
lucia-catu <cat24206@uvg.edu.gt>
2026-04-15 10:29:41 -0600

    prelab5

---
+++ Prelab5/Prelab5/main.c
@@ -0,0 +1,18 @@
+/*
+ * Prelab5.c
+ *
+ * Created: 15/04/2026 10:29:16
+ * Author : eacat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
